974cfb8e0a8e4cdf58af9e42732dcdf14b457fcf
AbdulazizRus <coderdim@gmail.com>
2024-07-19 22:11:56 +0500

    firebasse created

---
+++ lib/firebase.ts
@@ -0,0 +1,15 @@
+import { getApp, getApps, initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLICK_FIREBASE_API_KEY,
+  authDomain: 'drive-87d82.firebaseapp.com',
+  projectId: 'drive-87d82',
+  storageBucket: 'drive-87d82.appspot.com',
+  messagingSenderId: '6088705963',
+  appId: '1:6088705963:web:f145b3123f570e180d74ee',
+}
+
+!getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore()
+export { db }
