a5019d006643ac2feedb55d43e2159e5cd77794b
AbdulazizRus <coderdim@gmail.com>
2024-07-31 15:49:38 +0500

    AS

--- components/card/plan-card.tsx
+++ components/card/plan-card.tsx
@@ -1,3 +1,7 @@
+import { Check } from 'lucide-react'
+import { Button } from '../ui/button'
+import { Separator } from '../ui/separator'
+
 interface PlanCardProps {
   name: string
   description: string
@@ -23,6 +27,20 @@ const PlanCard = ({
           {price}
         </span>
         <span className="text-gray-500 dark:text-gray-400">/month</span>
+      </div>
+      <div className="w-full flex justify-center">
+        <Button>Get offer</Button>
+      </div>
+      <Separator className="mt-4" />
+      <p className="mt-3 opacity-75 text-sm">Google One includes</p>
+
+      <div className="flex flex-col space-y-2 mt-4">
+        {options.split(',').map((o) => (
+          <div className="flex items-center">
+            <Check className="mr-2" size={16} />
+            <span className="text-sm">{o}</span>
+          </div>
+        ))}
       </div>
     </div>
   )
--- components/modals/plan-modal.tsx
+++ components/modals/plan-modal.tsx
@@ -42,6 +42,5 @@ const planArray = [
     price: '10',
     options:
       '100 GB of storage, Access to Google experts,  Share with up to 5 others, Extra member benefits, More Google Photos editing features',
-    priceId: 'price_1OK5jiL7w2jHXlsSaWtc7yQI',
   },
 ]
